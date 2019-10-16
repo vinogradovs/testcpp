6ca8b444733c0d0eeccff6e3fa517011aa6bfdd4
Sergey Vinogradov <Sergey_Vinogradov1@epam.com>
2019-10-16 09:13:35 +0200

    initial commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, world, from Visual C++!" << endl;
+    return 0;
+}
+
