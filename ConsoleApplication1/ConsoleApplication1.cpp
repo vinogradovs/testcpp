329735fb8eb38b85de21232d59eab8e23515e6d7
Sergey Vinogradov <Sergey_Vinogradov1@epam.com>
2019-10-16 09:40:55 +0200

    test array index out of bounds analysis result

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,6 +8,12 @@ using namespace std;
 int main()
 {
 	cout << "Hello, world, from Visual C++!" << endl;
+	int arr[] = { 1,2,3,4,5 };
+	printf("arr [0] is %d\n", arr[0]);
+
+	// arr[10] is out of bound 
+	printf("arr[10] is %d\n", arr[10]);
+	return 0;
     return 0;
 }
 
