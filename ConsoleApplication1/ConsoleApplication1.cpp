298b0514cc34bda4ae3e09b675ad3c86e784ff33
Sergey Vinogradov <Sergey_Vinogradov1@epam.com>
2019-10-16 09:59:46 +0200

    test 2

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,7 +13,32 @@ int main()
 
 	// arr[10] is out of bound 
 	printf("arr[10] is %d\n", arr[10]);
+	
 	return 0;
-    return 0;
+	
+	
+    
 }
 
+int getValueFromArray(int *array, int len, int index) {
+
+	int value;
+
+	// check that the array index is less than the maximum 
+
+	// length of the array 
+	if (index < len) {
+
+		// get the value at the specified index of the array 
+		value = array[index];
+	}
+	// if array index is invalid then output error message 
+
+	// and return value indicating error 
+	else {
+		printf("Value is: %d\n", array[index]);
+		value = -1;
+	}
+
+	return value;
+}
